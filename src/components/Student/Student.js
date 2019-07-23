604d4726b12a0f017b7fab326d408499fdeec647
Blake Trapnell <Blake.Trapnell103@gmail.com>
2019-07-23 16:18:40 -0600

    Finished

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+import axios from "axios"
 
 export default class Student extends Component {
   constructor() {
     super()
-
+    this.state = {
+      studentInfo: {}
+    }
   }
+componentDidMount() {
+return axios.get(`http://localhost:3005/students/${this.props.match.params.id}`)
+.then((res)=> this.setState({studentInfo: res.data}))
 
+}
   render() {
     return (
       <div className="box">
         <h1>Student</h1>
+        <h3>Grade: {this.state.studentInfo.grade}</h3>
+        <h3>Email: {this.state.studentInfo.email}</h3>
       </div>
     )
   }
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Route, Switch} from "react-router-dom";
+import Home from "./components/Home/Home.js";
+import About from "./components/About/About.js";
+import ClassList from "./components/ClassList/ClassList"
+import Student from "./components/Student/Student"
+
+export default (
+
+    <Switch>
+<Route component= {Home} exact path= "/"/>
+<Route component= {About} path= "/about"/>
+<Route component= {ClassList} path = "/classlist/:class"/>
+<Route component= {Student} path = "/student/:id"/>
+
+    </Switch>
+)
